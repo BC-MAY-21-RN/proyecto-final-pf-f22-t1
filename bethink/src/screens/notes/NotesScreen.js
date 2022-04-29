16a037870c973e2af4805eaddfa9586c3435a29a
jonathan <yoni_12rrs@hotmail.com>
2022-04-28 19:06:28 -0500

    Notes home, indicators, cards, and btn float done,
    Co-authored-by: Carolina <carolinaramon239578@correo.itm.edu.co>
    Co-authored-by: Jonathan <yoni_12rrs@hotmail.com>
    Co-authored-by: Cris <crprmahdz@yahoo.com>
    Co-authored-by: Omar <ing.omar.cardona@gmail.com>

--- bethink/src/screens/notes/NotesScreen.js
+++ bethink/src/screens/notes/NotesScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {ButtonAddNotes} from '../../components/notes/ButtonAddNotes';
+import {CardNotes} from '../../components/notes/CardNotes';
+import {PriorityNotes} from '../../components/notes/PriorityNotes';
 
 export const NotesScreen = () => {
   return (
-    <View>
-      <Text>Hola</Text>
+    <View style={styles.containerMain}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <View style={styles.containerCenter}>
+            <Text style={styles.title}>Notes</Text>
+          </View>
+          <PriorityNotes />
+          <CardNotes />
+        </View>
+      </ScrollView>
+      <ButtonAddNotes />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  containerMain: {
+    flex: 1,
+    backgroundColor: '#231E41',
+  },
+  container: {
+    marginHorizontal: 20,
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  containerCenter: {
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+});
