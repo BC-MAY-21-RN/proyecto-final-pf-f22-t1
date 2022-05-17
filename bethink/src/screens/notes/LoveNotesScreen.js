7594629f58d034bcb79c9b451923326c3c772e51
jonathan <yoni_12rrs@hotmail.com>
2022-05-17 17:06:16 -0500

    Show notes on love notes screen,
    Co-authored-by: Jonathan <yoni_12rrs@hotmail.com>
    Co-authored-by: Cris <crprmahdz@yahoo.com>

--- bethink/src/screens/notes/LoveNotesScreen.js
+++ bethink/src/screens/notes/LoveNotesScreen.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView, StyleSheet, View, Image, Text} from 'react-native';
+import {useSelector} from 'react-redux';
+import {ContainerMain} from '../../components/home/ContainerMain';
+import {LoveNotes} from '../../components/home/LoveNotes';
+
 export const LoveNotesScreen = () => {
+  const {notes} = useSelector(state => state.notes);
+  const notesLove = notes.filter(note => note.love === true);
   return (
-    <View>
-      <Text>Hola</Text>
-    </View>
+    <ContainerMain>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          {notesLove.length !== 0 ? (
+            <LoveNotes notes={notesLove} />
+          ) : (
+            <View style={styles.containerNoNotes}>
+              <Image
+                source={require('../../imgs/notes/nolovesnotes.png')}
+                style={styles.img}
+              />
+              <Text style={styles.message}>
+                You don't have any note with "love it"
+              </Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </ContainerMain>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  img: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+  },
+  message: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  containerNoNotes: {
+    alignSelf: 'center',
+    marginTop: 200,
+  },
+});
