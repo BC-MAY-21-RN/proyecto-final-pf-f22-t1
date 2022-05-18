3fbb1d630d56fb39979ab6498d5e2f368f375758
jonathan <yoni_12rrs@hotmail.com>
2022-05-17 19:52:25 -0500

    Love notes light mode,
    Co-authored-by: Jonathan <yoni_12rrs@hotmail.com>
    Co-authored-by: Cris <crprmahdz@yahoo.com>

--- bethink/src/screens/notes/LoveNotesScreen.js
+++ bethink/src/screens/notes/LoveNotesScreen.js
@@ -3,28 +3,22 @@ import {ScrollView, StyleSheet, View, Image, Text} from 'react-native';
 import {useSelector} from 'react-redux';
 import {ContainerMain} from '../../components/home/ContainerMain';
 import {LoveNotes} from '../../components/home/LoveNotes';
+import {SectionName} from '../../components/home/SectionName';
+import {ConditionalLoveNotes} from '../../components/loveNotes/ConditionalLoveNotes';
 import {ModalPinNote} from '../../components/notes/ModalPinNote';
 
 export const LoveNotesScreen = ({navigation}) => {
   const {notes} = useSelector(state => state.notes);
+  const {mode} = useSelector(state => state.ui);
   const notesLove = notes.filter(note => note.love === true);
   return (
     <ContainerMain>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
-          {notesLove.length !== 0 ? (
-            <LoveNotes notes={notesLove} navigation={navigation} />
-          ) : (
-            <View style={styles.containerNoNotes}>
-              <Image
-                source={require('../../imgs/notes/nolovesnotes.png')}
-                style={styles.img}
-              />
-              <Text style={styles.message}>
-                You don't have any note with "love it"
-              </Text>
-            </View>
-          )}
+          <Text style={[styles.title, mode === 'light' && styles.titleLight]}>
+            Love notes
+          </Text>
+          <ConditionalLoveNotes notesLove={notesLove} navigation={navigation} />
         </View>
       </ScrollView>
       <ModalPinNote navigation={navigation} />
@@ -37,18 +31,15 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginTop: 20,
   },
-  img: {
-    width: 150,
-    height: 150,
-    alignSelf: 'center',
-  },
-  message: {
+
+  title: {
     color: 'white',
+    fontSize: 30,
     fontWeight: 'bold',
-    marginTop: 10,
+    textAlign: 'center',
+    marginBottom: 20,
   },
-  containerNoNotes: {
-    alignSelf: 'center',
-    marginTop: 200,
+  titleLight: {
+    color: 'black',
   },
 });
